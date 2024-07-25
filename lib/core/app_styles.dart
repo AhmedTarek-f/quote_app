d3de08f5e7198331950ecf4bda686a61c7e20af8
AhmedTarek <o0ahmedtarek0o@gmail.com>
2024-07-25 15:03:30 +0300

    adding app text styles

---
+++ lib/core/app_styles.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:intern2grow_quote_app/constants.dart';
+
+abstract class AppStyles
+{
+  static TextStyle stylesRegular14 =const TextStyle(
+    fontSize: 14,
+    fontFamily: fontFamilyName,
+    fontWeight: FontWeight.w400,
+  );
+  static TextStyle stylesMedium14 =const TextStyle(
+    fontSize: 14,
+    fontFamily: fontFamilyName,
+    fontWeight: FontWeight.w500,
+  );
+  static TextStyle stylesSemiBold14 =const TextStyle(
+    fontSize: 14,
+    fontFamily: fontFamilyName,
+    fontWeight: FontWeight.w600,
+  );
+  static TextStyle stylesBold14 =const TextStyle(
+    fontSize: 14,
+    fontFamily: fontFamilyName,
+    fontWeight: FontWeight.w700,
+  );
+  static TextStyle stylesRegular20 =const TextStyle(
+    fontSize: 20,
+    fontFamily: fontFamilyName,
+    fontWeight: FontWeight.w400,
+  );
+  static TextStyle stylesMedium20 =const TextStyle(
+    fontSize: 20,
+    fontFamily: fontFamilyName,
+    fontWeight: FontWeight.w500,
+  );
+  static TextStyle stylesSemiBold20 =const TextStyle(
+    fontSize: 20,
+    fontFamily: fontFamilyName,
+    fontWeight: FontWeight.w600,
+  );
+  static TextStyle stylesBold20 =const TextStyle(
+    fontSize: 20,
+    fontFamily: fontFamilyName,
+    fontWeight: FontWeight.w700,
+  );
+}
