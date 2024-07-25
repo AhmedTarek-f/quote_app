45de4410b20ee6337201f9bd8b353130b759977e
AhmedTarek <o0ahmedtarek0o@gmail.com>
2024-07-25 15:02:34 +0300

    adding app images router

---
+++ lib/core/app_images.dart
@@ -0,0 +1,8 @@
+abstract class AppImages
+{
+  static const String authTopBackGroundImage = "assets/images/auth_top_background.png";
+  static const String intern2GrowImage = "assets/images/intern_2_grow_rect.png";
+  static const String profileImage = "assets/images/profile_img.png";
+  static const String hideIcon = "assets/images/hide_Icon.png";
+  static const String removeIcon = "assets/images/remove_icon.png";
+}
