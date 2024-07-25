c9510bde875cb6f871221ec6c27efb64a183d780
AhmedTarek <o0ahmedtarek0o@gmail.com>
2024-07-25 16:06:42 +0300

    adding navigation to the registration view

--- lib/features/log_in/presentation/view/widgets/register_field.dart
+++ lib/features/log_in/presentation/view/widgets/register_field.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:intern2grow_quote_app/core/app_router.dart';
 import 'package:intern2grow_quote_app/core/app_styles.dart';
 
 class RegisterField extends StatelessWidget {
@@ -15,13 +17,18 @@ class RegisterField extends StatelessWidget {
           style: AppStyles.stylesRegular14.copyWith(color:const Color(0xff2A2B2E)),
         ),
         const SizedBox(width: 6,),
-        Container(
-          decoration: const BoxDecoration(
-              border: Border(bottom: BorderSide(style: BorderStyle.solid,color: Color(0xff4F90F0) ,width: 1.5))
-          ),
-          child: Text(
-            "Register",
-            style: AppStyles.stylesBold14.copyWith(color:const Color(0xff2A2B2E)),
+        InkWell(
+          onTap: (){
+            GoRouter.of(context).pushReplacement(AppRouter.kRegisterView);
+          },
+          child: Container(
+            decoration: const BoxDecoration(
+                border: Border(bottom: BorderSide(style: BorderStyle.solid,color: Color(0xff4F90F0) ,width: 1.5))
+            ),
+            child: Text(
+              "Register",
+              style: AppStyles.stylesBold14.copyWith(color:const Color(0xff2A2B2E)),
+            ),
           ),
         )
       ],
